99dd63e67f4c6dbc53fa73f762c9575737b13cf0
Nils Lenoch <84383470+NL27@users.noreply.github.com>
2021-12-18 15:31:11 +0100

    Add files via upload

---
+++ minitalk/client.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlenoch <nlenoch@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 14:35:01 by rrajaobe          #+#    #+#             */
+/*   Updated: 2021/12/17 22:08:13 by nlenoch          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+int	ft_strlen(const char *c)
+{
+	int	count;
+
+	count = 0;
+	while (*c != '\0')
+	{
+		count++;
+		c++;
+	}
+	return (count);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	sign;
+
+	result = 0;
+	sign = 1;
+	while ((*str == 32) || (*str >= 9 && *str <= 13))
+		str++;
+	if ((*str >= 65 && *str <= 90) || (*str >= 97 && *str <= 122))
+		return (0);
+	if ((*str) == '-' || (*str) == '+')
+	{
+		if (*str == '+')
+			sign = 1;
+		else
+			sign = -1;
+		++str;
+	}
+	while ((*str >= 48) && (*str <= 57))
+	{
+		result = (result * 10) + (*str - 48);
+		str++;
+	}
+	return (sign * result);
+}
+
+void	send_newline(int pid)
+{
+	char	a;
+	int		n;
+
+	a = '\n';
+	n = 1;
+	while (n <= 128)
+	{
+		if (a & n)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		n *= 2;
+		usleep(100);
+	}
+}
+
+void	send_chars(int pid, char **argv)
+{
+	int	len;
+	int	n;
+	int	i;
+
+	len = ft_strlen(argv[2]);
+	i = 0;
+	while (i < len)
+	{
+		n = 1;
+		while (n <= 128)
+		{
+			if (argv[2][i] & n)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			n *= 2;
+			usleep(100);
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		pid;
+
+	if (argc != 3)
+		return (0);
+	pid = ft_atoi(argv[1]);
+	send_chars(pid, argv);
+	send_newline(pid);
+}
---
+++ minitalk/server.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlenoch <nlenoch@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 14:35:09 by rrajaobe          #+#    #+#             */
+/*   Updated: 2021/12/17 22:44:40 by nlenoch          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	c;
+
+	if (n < 0)
+	{
+		write(fd, "-", 1);
+		if (n == -2147483648)
+		{
+			write(fd, "2147483648", 10);
+			return ;
+		}
+		n *= -1;
+	}
+	if (n > 9)
+		ft_putnbr_fd(n / 10, fd);
+	c = (n % 10) + '0';
+	write(fd, &c, 1);
+}
+
+void	transform(int s)
+{
+	static int	c = 0;
+	static int	count = 1;
+
+	if (s == SIGUSR1)
+		c += count;
+	count *= 2;
+	if (count == 256)
+	{
+		count = 1;
+		write(1, &c, 1);
+		c = 0;
+	}
+}
+
+int	main(void)
+{
+	int	pid;
+
+	pid = getpid();
+	write(1, "PID: ", 5);
+	ft_putnbr_fd(pid, 1);
+	write(1, "\n", 1);
+	while (1)
+	{
+		signal(SIGUSR1, transform);
+		signal(SIGUSR2, transform);
+		pause();
+	}
+	return (0);
+}
